a1243c85a5d4cc3faa435a0084b37eb80dea22b6
Dmitriy Grishin <dagrishin.dev@gmail.com>
2023-01-12 23:30:10 +0300

    add settings config

--- db/session.py
+++ db/session.py
@@ -4,10 +4,10 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from db.config import REAL_DATABASE_URL
+from src.config import settings
 
 # create async engine for interaction with database
-engine = create_async_engine(REAL_DATABASE_URL, future=True, echo=True)
+engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
 
 # create session for the interaction with database
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
