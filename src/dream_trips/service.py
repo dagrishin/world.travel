c3500ac78e9b064b9546fe329b289b886f4d4782
Dmitriy Grishin <dagrishin.dev@gmail.com>
2023-01-14 01:22:11 +0300

    add DAL Likes

--- src/dream_trips/service.py
+++ src/dream_trips/service.py
@@ -1,11 +1,13 @@
-from typing import List
+from typing import List, Any
+from uuid import UUID
 
 from fastapi import HTTPException
+from sqlalchemy import delete as sqlalchemy_delete
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette import status
 from sqlalchemy.future import select
+from starlette import status
 
-from src.dream_trips.models import DreamTrips
+from src.dream_trips.models import DreamTrips, Likes
 from src.service import BaseDAL
 
 
@@ -37,3 +39,28 @@ class DreamTripsDAL(BaseDAL):
         queryset = await self.db_session.execute(query)
         dreams = queryset.scalars().all()
         return dreams
+
+
+class LikesDAL(BaseDAL):
+    def init(self, db_session: AsyncSession):
+        super().__init__(db_session, Likes)
+
+    async def create(self, obj_in: dict) -> Any:
+        new_obj = self._model(**obj_in)
+        self.db_session.add(new_obj)
+        try:
+            await self.db_session.flush()
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to create object.")
+        return new_obj
+
+    async def delete(self, user_id: UUID, dream_trip_id: int):
+        query = sqlalchemy_delete(self._model).where(self._model.user_id == user_id,
+                                                     self._model.dream_trip_id == dream_trip_id)
+        await self.db_session.execute(query)
+        try:
+            await self.db_session.commit()
+        except Exception as e:
+            await self.db_session.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to delete object.")
+        return True
